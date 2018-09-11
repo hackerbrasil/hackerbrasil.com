7b35dfde84fe7690fd7974f940ab22e95a2fe6c5
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-09-11 16:01:10 -0300

    M  resources/views/welcome.php

--- resources/views/welcome.php
+++ resources/views/welcome.php
@@ -56,7 +56,7 @@
   </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script src="js/keynavigator.js"></script>
+    <script src="https://cdn.rawgit.com/hackerbrasil/hackerbrasil/master/public/js/keynavigator.min.js"></script>
     <script type="text/javascript">
     //https://github.com/nekman/keynavigator
       var rows=$('table > tbody tr').keynavigator();
