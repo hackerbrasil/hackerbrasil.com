ec1a34462817f6c9ee47f64ab5a42d412351de1a
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-09-11 16:29:00 -0300

    M  app/Http/Controllers/UserController.php M  resources/views/home.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,20 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     function index(){
-        return view('home');
+        $data=[
+            'links'=>[
+                [
+                    'site'=>'Gizmodo Brasil',
+                    'title'=>'Noticia 1',
+                    'created_at'=>time()
+                ],
+                [
+                    'site'=>'Tecmundo',
+                    'title'=>'Noticia 2',
+                    'created_at'=>time()
+                ]
+            ]
+        ];
+        return view('home',$data);
     }
 }
--- resources/views/home.php
+++ resources/views/home.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt" dir="ltr">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Hacker Brasil</title>
@@ -8,80 +8,73 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <style media="screen">
     .active {
-      background: #69f;
-      color: #fff;
-      text-decoration: none;
-      border-radius: 4px;
+        background: #69f;
+        color: #fff;
+        text-decoration: none;
+        border-radius: 4px;
     }
     </style>
-  </head>
-  <body>
+</head>
+<body>
     <h1>Hacker Brasil</h1>
     <div class="table-responsive-sm">
-    <table class="table table-sm">
-      <thead class="thead-light">
-        <tr>
-          <th scope="col">Site</th>
-          <th scope="col">Artigo</th>
-          <th scope="col" colspan="2">Data</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr id="primeiraLinha">
-          <th scope="row">Gizmodo Brasil</th>
-          <td>Trump ameaça Google e Twitter</td>
-          <td>2 horas atrás</td>
-          <td class="text-right align-middle">
-            <i class="fas fa-times"></i>
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">Gizmodo Brasil</th>
-          <td>Trump ameaça Google e Twitter</td>
-          <td>2 horas atrás</td>
-          <td class="text-right align-middle">
-            <i class="fas fa-times"></i>
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">Gizmodo Brasil</th>
-          <td>Trump ameaça Google e Twitter</td>
-          <td>2 horas atrás</td>
-          <td class="text-right align-middle">
-            <i class="fas fa-times"></i>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script src="https://cdn.rawgit.com/hackerbrasil/hackerbrasil/master/public/js/keynavigator.min.js"></script>
-    <script type="text/javascript">
-    //https://github.com/nekman/keynavigator
-      var rows=$('table > tbody tr').keynavigator();
-      rows.keynavigator.setActive($('#primeiraLinha'));
+        <table class="table table-sm">
+            <thead class="thead-light">
+                <tr>
+                    <th scope="col">Site</th>
+                    <th scope="col">Artigo</th>
+                    <th scope="col" colspan="2">Data</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $i=1;
+                foreach($links as $link){
+                    if($i++==1){
+                        print '<tr id="primeiraLinha">';
+                    }else{
+                        print '<tr>';
+                    }
+                    print '<th scope="row">'.$link['site'].'</th>';
+                    print '<td>'.$link['title'].'</td>';
+                    print '<td>'.$link['created_at'].'</td>';
+                    print '<td class="text-right align-middle">';
+                    print '<i class="fas fa-times"></i>';
+                    print '</td>';
+                    print '</tr>';
+                }
+                ?>
+                </tbody>
+                </table>
+                </div>
+                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+                <script src="https://cdn.rawgit.com/hackerbrasil/hackerbrasil/master/public/js/keynavigator.min.js"></script>
+                <script type="text/javascript">
+                //https://github.com/nekman/keynavigator
+                var rows=$('table > tbody tr').keynavigator();
+                rows.keynavigator.setActive($('#primeiraLinha'));
 
-//       $(function() {
-//   var $rows = $('table > tbody tr').keynavigator({
-//     // Don't activate on anything...
-//     activateOn: ''
-//   });
-//
-//   // Handle activation your self.
-//   $rows.find('td').on('click', function(e) {
-//     var $input = $(this),
-//         $selectedRow = $input.closest('tr');
-//
-//     $rows.keynavigator.setActive($selectedRow);
-//
-//     // Keep focus on input
-//     $input.focus();
-//
-//     // Stop event bubbling
-//     e.stopPropagation();
-//   });
-// });
-    </script>
-  </body>
-</html>
+                //       $(function() {
+                //   var $rows = $('table > tbody tr').keynavigator({
+                //     // Don't activate on anything...
+                //     activateOn: ''
+                //   });
+                //
+                //   // Handle activation your self.
+                //   $rows.find('td').on('click', function(e) {
+                //     var $input = $(this),
+                //         $selectedRow = $input.closest('tr');
+                //
+                //     $rows.keynavigator.setActive($selectedRow);
+                //
+                //     // Keep focus on input
+                //     $input.focus();
+                //
+                //     // Stop event bubbling
+                //     e.stopPropagation();
+                //   });
+                // });
+                </script>
+                </body>
+                </html>
