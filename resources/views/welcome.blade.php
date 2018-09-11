a2214e5fc5a4bb2725b11848cd9c384ac07b3cd3
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-09-11 15:47:04 -0300

    A  .env.example A  .gitattributes A  .gitignore A  .todo A  app/Console/Kernel.php A  app/Exceptions/Handler.php A  app/Http/Controllers/Auth/ForgotPasswordController.php A  app/Http/Controllers/Auth/LoginController.php A  app/Http/Controllers/Auth/RegisterController.php A  app/Http/Controllers/Auth/ResetPasswordController.php A  app/Http/Controllers/Controller.php A  app/Http/Kernel.php A  app/Http/Middleware/EncryptCookies.php A  app/Http/Middleware/RedirectIfAuthenticated.php A  app/Http/Middleware/TrimStrings.php A  app/Http/Middleware/TrustProxies.php A  app/Http/Middleware/VerifyCsrfToken.php A  app/Providers/AppServiceProvider.php A  app/Providers/AuthServiceProvider.php A  app/Providers/BroadcastServiceProvider.php A  app/Providers/EventServiceProvider.php A  app/Providers/RouteServiceProvider.php A  app/User.php A  artisan A  bootstrap/app.php A  bootstrap/cache/.gitignore A  composer.json A  composer.lock A  config/app.php A  config/auth.php A  config/broadcasting.php A  config/cache.php A  config/database.php A  config/filesystems.php A  config/mail.php A  config/queue.php A  config/services.php A  config/session.php A  config/view.php A  database/.gitignore A  database/factories/UserFactory.php A  database/migrations/2014_10_12_000000_create_users_table.php A  database/migrations/2014_10_12_100000_create_password_resets_table.php A  database/seeds/DatabaseSeeder.php A  package.json A  phpunit.xml A  public/.htaccess A  public/css/app.css A  public/favicon.ico A  public/index.php A  public/js/app.js A  public/js/keynavigator.js A  public/robots.txt A  public/web.config A  readme.md A  resources/assets/js/app.js A  resources/assets/js/bootstrap.js A  resources/assets/js/components/ExampleComponent.vue A  resources/assets/sass/_variables.scss A  resources/assets/sass/app.scss A  resources/lang/en/auth.php A  resources/lang/en/pagination.php A  resources/lang/en/passwords.php A  resources/lang/en/validation.php A  resources/views/welcome.blade.php A  routes/api.php A  routes/channels.php A  routes/console.php A  routes/web.php A  server.php A  storage/app/.gitignore A  storage/app/public/.gitignore A  storage/framework/.gitignore A  storage/framework/cache/.gitignore A  storage/framework/sessions/.gitignore A  storage/framework/testing/.gitignore A  storage/framework/views/.gitignore A  storage/logs/.gitignore A  tests/CreatesApplication.php A  tests/Feature/ExampleTest.php A  tests/TestCase.php A  tests/Unit/ExampleTest.php A  webpack.mix.js

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="pt" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Hacker Brasil</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <style media="screen">
+    .active {
+      background: #69f;
+      color: #fff;
+      text-decoration: none;
+      border-radius: 4px;
+    }
+    </style>
+  </head>
+  <body>
+    <h1>Hacker Brasil</h1>
+    <div class="table-responsive-sm">
+    <table class="table table-sm">
+      <thead class="thead-light">
+        <tr>
+          <th scope="col">Site</th>
+          <th scope="col">Artigo</th>
+          <th scope="col" colspan="2">Data</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr id="primeiraLinha">
+          <th scope="row">Gizmodo Brasil</th>
+          <td>Trump ameaça Google e Twitter</td>
+          <td>2 horas atrás</td>
+          <td class="text-right align-middle">
+            <i class="fas fa-times"></i>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">Gizmodo Brasil</th>
+          <td>Trump ameaça Google e Twitter</td>
+          <td>2 horas atrás</td>
+          <td class="text-right align-middle">
+            <i class="fas fa-times"></i>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">Gizmodo Brasil</th>
+          <td>Trump ameaça Google e Twitter</td>
+          <td>2 horas atrás</td>
+          <td class="text-right align-middle">
+            <i class="fas fa-times"></i>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="js/keynavigator.js"></script>
+    <script type="text/javascript">
+    //https://github.com/nekman/keynavigator
+      var rows=$('table > tbody tr').keynavigator();
+      rows.keynavigator.setActive($('#primeiraLinha'));
+
+//       $(function() {
+//   var $rows = $('table > tbody tr').keynavigator({
+//     // Don't activate on anything...
+//     activateOn: ''
+//   });
+//
+//   // Handle activation your self.
+//   $rows.find('td').on('click', function(e) {
+//     var $input = $(this),
+//         $selectedRow = $input.closest('tr');
+//
+//     $rows.keynavigator.setActive($selectedRow);
+//
+//     // Keep focus on input
+//     $input.focus();
+//
+//     // Stop event bubbling
+//     e.stopPropagation();
+//   });
+// });
+    </script>
+  </body>
+</html>
