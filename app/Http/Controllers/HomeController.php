84d2ccca6bf900e48bab07ee910cff5ba77d75ef
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-09-12 17:05:20 -0300

    M  app/Http/Controllers/HomeController.php M  config/app.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,17 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\LinksController;
-use App\Http\Controllers\Medoo;
+use Medoo;
 
 class HomeController extends Controller
 {
     function mostrarAViewHome(){
         $db = new Medoo();
         var_dump($db->info());
-        // $LinksController=new LinksController();
-        // $data=[
-        //     'links'=>$LinksController->lerOsLinksNoBancoDeDados()
-        // ];
-        // return view('home',$data);
+        $LinksController=new LinksController();
+        $data=[
+            'links'=>$LinksController->lerOsLinksNoBancoDeDados()
+        ];
+        return view('home',$data);
     }
 }
--- config/app.php
+++ config/app.php
@@ -210,6 +210,7 @@ return [
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
+        'Medoo' => App\Http\Controllers\Medoo::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
         'Password' => Illuminate\Support\Facades\Password::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
