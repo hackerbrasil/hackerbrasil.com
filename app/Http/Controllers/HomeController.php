94c6b0e8255557524f64386a1016f1320725b35d
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-09-20 16:17:19 -0300

    M  app/Http/Controllers/HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,6 +12,27 @@ class HomeController extends Controller
         $data=[
             'links'=>$LinksController->lerOsLinksNoBancoDeDados()
         ];
-        return view('home',$data);
+        $userToken=request()->cookie('user_token');
+        if($userToken){
+            return response()
+            ->view('home',$data);
+        }else{
+            $umDia=24*60;
+            $umAno=365*$umDia;
+            $minutes=2*$umAno;
+            return response()
+            ->view('home',$data)
+            ->cookie('user_token', $this->criarUserToken(), $minutes);
+        }
+    }
+    function criarUserToken(){
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
+        $characters.= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ_-';
+        $limit=strlen($characters);
+        $uid = '';
+        for ($i = 0; $i < $limit; $i++) {
+            $uid .= $characters[rand(0, mb_strlen($characters)-1)];
+        }
+        return $uid.'_'.time();
     }
 }
