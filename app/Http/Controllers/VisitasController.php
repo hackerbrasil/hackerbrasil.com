63ecd5d213c4201141b8ea0abd07732c2854ca85
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-09-21 09:49:01 -0300

    M  app/Http/Controllers/VisitasController.php M  app/Http/Models/links

--- app/Http/Controllers/VisitasController.php
+++ app/Http/Controllers/VisitasController.php
@@ -9,13 +9,25 @@ use Medoo;
 class VisitasController extends Controller
 {
     function salvarVisita($urlHash,$type='view'){
+        $db=Medoo::connect();
         $type=strtolower($type);
-        if($type<>'view'){
+        if($type=='view'){
+            $where=[
+                'url_hash'=>$urlHash
+            ];
+            $link=$db->get('links','*',$where);
+            if(@$link['visitas']>0){
+                $visitas=$link['visitas']+1;
+            }else{
+                $visitas=1;
+            }
+            $data['visitas']=$visitas;
+            $db->update('links',$data,$where);
+        }else{
             $type='skip';
         }
         $userHash=request()->cookie('user_hash');
         if(strlen($userHash)>=21){
-            $db=Medoo::connect();
             $data=[
                 'url_hash'=>$urlHash,
                 'user_hash'=>$userHash,
