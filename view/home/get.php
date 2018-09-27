56d8abe835e3955eee5d5927f2e9be9f52706061
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-09-27 16:44:02 -0300

    A  .gitignore M  view/home/get.php

--- view/home/get.php
+++ view/home/get.php
@@ -5,11 +5,12 @@
     <title>Hacker Gaucho</title>
 </head>
 <body>
+    atribuir um indice ao up e outro ao down representando o primero e o ultimo id da lista<br>
     <a href="javascript:upLinks();">Up</a>
     <ul id="links">
 
     </ul>
-    <a href="javascript:downLinks();">Up</a>
+    <a href="javascript:downLinks();">Down</a>
     <script src="/js/jquery-3.3.1.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>
     <script src="/js/script.js"></script>
 </body>
