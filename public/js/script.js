91fd56cb648bbe5d5c071fd937cbf3548ced001f
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-10-03 16:57:49 -0300

    A  public/js/js.cookie.min.js M  public/js/script.js M  view/home/get.php

--- public/js/script.js
+++ public/js/script.js
@@ -7,27 +7,35 @@ var pageSize;
 
 //funções diversas
 function  atualizarLinks(){
-    var url='/ajax_links?linkId='+linkId+'&pageSize='+pageSize;
+    var url='/ajax_links?linkId='+getLinkId()+'&pageSize='+getPageSize();
+    console.log('Baixando url = '+url);
     $.get(url, function(links, status){
         exibirLinks(links);
     });
 }
 
 function downPage(){
-    linkId=linkId+pageSize;
+    //1) define linkIdValue=linkId+pageSize
+    var linkIdValue=linkId+pageSize;
+    //2) verifica se o linkIdValue existe via ajax
+    var url='/ajax_link_existe?linkId='+linkIdValue;
+    $.get(url, function(existe, status){
+        if(existe){
+        //2.true) define linkId=linkIdValue
+        setLinkId(linkIdValue);
+        }else{
+        //2.false) define linkId=defaultLinkId
+        setLinkid(defaultLinkId);
+        }
+    });
+    //3) atualiza a página
     atualizarLinks();
 }
 
 function exibirLinks(links){
-    if(links==false){
-        if(getLinkId()<defaultLinkId){
-            //TODO pedir via ajax o id do último link adicionado, setar o link e em seguida atualizar os links
-            setLinkId(defaultLinkId);
-        }else{
-            setLinkId(defaultLinkId);
-            atualizarLinks();
-        }
-    }else{
+    //1) processa o resultado
+    if(links){
+        //2.true) imprime os links e atualiza o cookie
         var i=0;
         var text='';
         while (links[i]) {
@@ -35,11 +43,26 @@ function exibirLinks(links){
             i++;
         }
         $('#links').html(text);
+        setLinkIdCookie(linkId);
+    }else{
+        //2.false) define o linkId com o valor default e atualiza o cookie
+        setLinkId(defaultLinkId);
+        setLinkIdCookie(defaultLinkId);
     }
 }
 
 function upPage(){
-    linkId=linkId-pageSize;
+    //1) verifica se o valor do linkId é maior ou igual ao valor default do linkId
+    if(linkId>=defaultLinkId){
+        //1.true) define linkId=linkId-pageSize
+        var linkIdValue=linkId-pageSize;
+    }else{
+        //1.false) define linkId=defaultValue
+        var linkIdValue=defaultLinkId;
+    }
+    //2) setar linkId
+    setLinkId(linkIdValue);
+    //3) atualizar links
     atualizarLinks();
 }
 
@@ -48,17 +71,29 @@ function getLinkId(){
     return linkId;
 }
 
+function getLinkIdCookie(){
+    return Cookies.get('linkId');
+}
+
 function getPageSize(){
     return pageSize;
 }
 
 //set
+function setLinkIdCookie(linkIdCookieValue){
+    //https://github.com/js-cookie/js-cookie
+    var doisAnos=365*2;
+    Cookies.remove('linkId');
+    Cookies.set('linkId',linkIdCookieValue,{ expires: doisAnos });
+}
+
 function setLinkId(linkIdValue){
     if(linkIdValue>0){
         linkId=linkIdValue;
     }else{
-        linkId=defaultIdValue;
+        linkId=defaultLinkId;
     }
+    console.log("Link = "+linkId);
 }
 
 function setPageSize(pageSizeValue){
@@ -67,11 +102,22 @@ function setPageSize(pageSizeValue){
     }else{
         pageSize=defaultPageSize;
     }
+    console.log("Tamanho da página = "+pageSize);
 }
 
 //load
 $(function(){
-    setLinkId(1);
+    //1) define o pageSize
     setPageSize(3);
+    //2) verifica se existe um cookie
+    var linkIdCookieValue=getLinkIdCookie();
+    if(linkIdCookieValue){
+        //2.true) define o linkId com o valor do cookie
+        setLinkId(linkIdCookieValue);
+    }else{
+        //2.false) defina o linkId com o valor default
+        setLinkId(defaultLinkId);
+    }
+    //3) baixar o link id e exibir os links
     atualizarLinks();
 });
--- view/home/get.php
+++ view/home/get.php
@@ -11,6 +11,7 @@
     </ul>
     <a href="javascript:downPage();">Down page</a>
     <script src="/js/jquery-3.3.1.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>
+<script src="/js/js.cookie.min.js" integrity="sha384-8kKqrPvADL3/ZYJGtcqeG+fveXaJxTaI7LF1/a9QGZpkRVZSurP858KKE6rtnLLs" crossorigin="anonymous"></script>
     <script src="/js/script.js"></script>
 </body>
 </html>
