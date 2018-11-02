82fb68464ba29f01814e89066c5f6dcbeff02baf
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-11-02 16:07:54 -0300

    A  public/bootstrap-2.3.2.min.css A  public/jquery-3.3.1.min.js M  view/home.php

--- view/home.php
+++ view/home.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hacker Gaucho</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.2/css/bootstrap.min.css" integrity="sha256-GGxA0G/BODBJenufQrwUU4wbf6C5hWDHkR5uGpNfdp0=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="/bootstrap-2.3.2.min.css" integrity="sha256-GGxA0G/BODBJenufQrwUU4wbf6C5hWDHkR5uGpNfdp0=" crossorigin="anonymous" />
     <link rel="stylesheet" href="/style.css">
 </head>
 <body>
@@ -20,7 +20,7 @@
             </div>
         </div>
     </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="/jquery.appear.js"></script>
     <script src="/jquery.scrolling.js"></script>
     <script src="/script.js"></script>
