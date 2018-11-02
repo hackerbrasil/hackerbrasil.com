deda62bfeefcab65d377993d3f60e7018f362567
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-11-02 16:19:08 -0300

    R  public/bootstrap-2.3.2.min.css -> public/css/bootstrap-2.3.2.min.css A  public/css/bootstrap-responsive-2.3.2.min.css M  view/home.php

--- view/home.php
+++ view/home.php
@@ -4,8 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hacker Gaucho</title>
-    <link rel="stylesheet" href="/bootstrap-2.3.2.min.css" integrity="sha256-GGxA0G/BODBJenufQrwUU4wbf6C5hWDHkR5uGpNfdp0=" crossorigin="anonymous" />
+        <link rel="stylesheet" href="/bootstrap-responsive-2.3.2.min.css" integrity="sha256-lIFJvX0EbEpgbhha1orNrXaV1TKngfVWrIbwgcHl80E=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="/css/bootstrap-2.3.2.min.css" integrity="sha256-GGxA0G/BODBJenufQrwUU4wbf6C5hWDHkR5uGpNfdp0=" crossorigin="anonymous" />
     <link rel="stylesheet" href="/style.css">
+
 </head>
 <body>
     <div class="container">
