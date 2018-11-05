da5c20b81af3bd03f94ffeae06dd80a1b737c784
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-11-05 10:44:05 -0200

    M  view/home.php

--- view/home.php
+++ view/home.php
@@ -30,7 +30,6 @@
     <!-- Matomo -->
     <script type="text/javascript">
     var _paq = _paq || [];
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
     _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
     (function() {
