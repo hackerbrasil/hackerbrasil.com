f2948c61057e4e7dd6b9ef6e5e60d4a489ee670d
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-11-05 10:40:00 -0200

    M  view/home.php

--- view/home.php
+++ view/home.php
@@ -27,5 +27,20 @@
     <script src="/jquery.appear.js"></script>
     <script src="/jquery.scrolling.js"></script>
     <script src="/script.js"></script>
+    <!-- Matomo -->
+    <script type="text/javascript">
+    var _paq = _paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u="//piwik.mushape.com/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+</script>
+<!-- End Matomo Code -->
 </body>
 </html>
