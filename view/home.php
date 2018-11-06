2326cb7f05f552ec1ce1d0184b45fae2507312bd
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-11-06 17:32:06 -0200

    M inc M  view/home.php

--- view/home.php
+++ view/home.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hacker Gaucho</title>
-
     <link rel="stylesheet" href="/css/bootstrap-2.3.2.min.css" integrity="sha256-GGxA0G/BODBJenufQrwUU4wbf6C5hWDHkR5uGpNfdp0=" crossorigin="anonymous" />
     <link rel="stylesheet" href="/bootstrap-responsive-2.3.2.min.css" integrity="sha256-lIFJvX0EbEpgbhha1orNrXaV1TKngfVWrIbwgcHl80E=" crossorigin="anonymous" />
     <link rel="stylesheet" href="/style.css">
@@ -27,7 +26,7 @@
     <script src="/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="/jquery.appear.js"></script>
     <script src="/jquery.scrolling.js"></script>
-    <script src="/script.js"></script>
+    <script src="/script.js?v1"></script>
     <!-- Matomo -->
     <script type="text/javascript">
     var _paq = _paq || [];
