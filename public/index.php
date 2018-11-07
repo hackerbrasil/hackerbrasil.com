350b7acf6ebde1452ae8b907497370d934c88611
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-11-07 16:29:30 -0200

    M  public/index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 //dependencias
 require '../inc/autoload.php';
-if(isDev() || isset($_GET['debug'])){
+if(isDev()){
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
