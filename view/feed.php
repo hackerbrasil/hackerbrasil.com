5cd071fc00808cbd75990b5998d34d914d460f7b
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-11-13 15:49:21 -0200

    M  view/feed.php

--- view/feed.php
+++ view/feed.php
@@ -1,12 +1,12 @@
-    <div class="modal-header">
-    	<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-    	<h3><?php print $feed['name']; ?></h3>
-    </div>
-    <div class="modal-body">
-        <div class="avisos"></div>
-        <h1 class="text-center">
-            <?php print $totalDeLinks; ?>
-            <small>
+<div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+    <h3><?php print $feed['name']; ?></h3>
+</div>
+<div class="modal-body">
+    <div class="avisos"></div>
+    <h1 class="text-center">
+        <?php print $totalDeLinks; ?>
+        <small>
             <?php
             if($totalDeLinks<2){
                 print 'link';
@@ -19,24 +19,21 @@
         /
         <?php
         $visitas=$feed['visitas'];
+        $msg='visita';
         if($visitas<1){
-            $visitas='Zero';
-            $msg=' visita';
-        }
-        if($visitas==1){
-            $msg=' visita';
+            $visitas='Nenhuma';
         }
         if($visitas>1){
-            $msg=' visitas';
+            $msg='visitas';
         }
-        $msg='<small>'.$msg.'</small>';
+        $msg=' <small>'.$msg.'</small>';
         $cliques=$visitas.$msg;
         print $cliques; ?>
-        </h1>
-    </div>
-    <div class="modal-footer">
-    	<button type="button" data-dismiss="modal" class="btn">
-            <i class="icon-remove"></i>
-            Fechar
-        </button>
-    </div>
+    </h1>
+</div>
+<div class="modal-footer">
+    <button type="button" data-dismiss="modal" class="btn">
+        <i class="icon-remove"></i>
+        Fechar
+    </button>
+</div>
