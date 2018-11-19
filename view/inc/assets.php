609fe910c337b94d11844620707e93a686e131ab
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-11-19 20:40:32 -0200

    M  view/inc/assets.php

--- view/inc/assets.php
+++ view/inc/assets.php
@@ -1,9 +1,12 @@
 <?php view('inc/bs2'); ?>
+<?php
+
+?>
 <link rel="stylesheet" href="/css/style.css?v1">
 <link rel="stylesheet" href="/css/bootstrap-modal.css">
 <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/img/flags/br.svg">
 <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/img/flags/br.svg">
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href=/img/flags/br.svg">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/img/flags/br.svg">
 <link rel="apple-touch-icon-precomposed" href="/img/flags/br.svg">
 <link rel="shortcut icon" href="/img/flags/br.svg">
-<meta property="og:image" content="https://<?php print $_SERVER["SERVER_NAME"];?>/img/flags/br.svg">
+<meta property="og:image" content="<?php print $_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["SERVER_NAME"];?>/img/flags/br.svg">
