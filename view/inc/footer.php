0f11218a9a806473ec923ecc10121453555f3a38
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-04-06 16:27:43 -0300

    M  view/inc/footer.php

--- view/inc/footer.php
+++ view/inc/footer.php
@@ -4,24 +4,3 @@
 <script src="/js/bootstrap-modalmanager.js"></script>
 <script src="/js/bootstrap-modal.js"></script>
 <script src="/js/script.js?v20"></script>
-<?php
-if(!isDev()){
-    print '<script type="text/javascript">';
-    ?>
-    <!-- Matomo -->
-    var _paq = _paq || [];
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function() {
-        var u="https://piwik.mushape.com/";
-        _paq.push(['setTrackerUrl', u+'piwik.php']);
-        _paq.push(['setSiteId', '1']);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-    })();
-    <!-- End Matomo Code -->
-    <?php
-    print '</script>';
-}
-?>
